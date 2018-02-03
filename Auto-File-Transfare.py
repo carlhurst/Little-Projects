7dec3b4c722c00fa4bbfad86651183c4c690600f
carlhurst <carl.hurst@outlook.com>
2018-02-03 09:54:12 +0400

    Finalisation of the project just some editing to sort
    asdf

--- Auto-File-Transfare.py
+++ Auto-File-Transfare.py
@@ -1,8 +1,10 @@
-import os, shutil, re
+import os, shutil, re, pprint
 
 count = 0
-avi = 0
-mp4 = 0
+avi, mp4 = 0, 0
+
+fileLocations = []
+newfilelocation= []
 
 for folderName, subFolders, filenames in os.walk(r'\\LINKSYS05548\New Volume\Movies'):
     print("\nThe current folder is " + folderName)
@@ -15,16 +17,28 @@ for folderName, subFolders, filenames in os.walk(r'\\LINKSYS05548\New Volume\Mov
 
             x = folderName + '\\' + filename
 
+
             size = os.path.getsize(x) / 1000000000
             print("File is {:0.2f} GB".format(size))
 
             if filename.split(".")[-1] ==  "avi":
                 avi += 1
+                # Copy and add the new location
+                fileLocations.append(folderName + "\\" + filename)
+                newfilelocation.append(folderName + "\\" + filename + " - " + 'C:\\Users\\Thor\\Videos\\' + filename)# TODO change the location of the file destination
+
             elif filename.split(".")[-1] == "mp4":
                 mp4 += 1
 
+# Print the statistics
 print('We have number of avi films:' + str(avi))
-
 print('We have number of mp4 films:' + str(mp4))
-
 print(avi + mp4)
+input("Press Enter to Copy the Films into the File location Specified")
+
+for f in fileLocations:
+    shutil.copy(fileLocations[f], 'C:\\Users\\Thor\\Videos') # TODO change the location of the file destination
+
+file = open("Location.txt", "w")
+for l in newfilelocation:
+    file.write(l + '\n')
